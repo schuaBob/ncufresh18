412fe4cca4da5da459d2537c69cb56cdce868b5c
余函倩 <you@example.com>
2018-07-03 20:52:43 +0800

    外觀

--- routes/documents.js
+++ routes/documents.js
@@ -5,5 +5,49 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('documents/index', { title: '新生必讀' });
 });
+$(document).ready(function(){
+  alert("又來");
+  $("#big").click(function(){
+    res.redirect('documents/index');
+  });
+  $("#young").click(function(){
+    res.redirect('documents/graduate');
+    alert("又來");
+  });
+})
+      
+router.post('/add', function(req, res, next) {
+  if ((!req.session.name) ){
+    res.redirect('/');
+    return;
+  }
+  new Blog({
+    Username:req.session.name,
+    Article:req.body.Content,
+    CreateDate: Date.now()
+  }).save(function(err){
+    if(err){
+      console.log('fail to save to DB.');
+      return;
+    }
+    console.log('Save to DB.');
+  });
+  res.redirect('/');
+  });
+
+router.post('/update/:id', function(req, res, next) {
+  if ((!req.params.id) ){
+    res.redirect('/');
+    return;
+  }
+  Blog.update({_id:req.params.id},{Article:req.body.Content},
+    function(err){
+      if(err)
+        console.log('fail to update article.');
+      else
+        console.log('Save to DB.');
+    });
+    res.redirect('/users/profile');
+    });
 
 module.exports = router;
