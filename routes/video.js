82823655673fe1932189bef33a2a10316391a834
徐曼妮 <vanishment99@g.ncu.edu.tw>
2018-07-03 20:46:57 +0800

    day2 rotate

--- routes/video.js
+++ routes/video.js
@@ -6,6 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('video/index', { title: '影音專區' });
 });
 
+router.get('/page', function(req, res, next) {
+  /*video.find({}).exec(function(err, video) {
+    var foodURL, housingURL, transportationURL, educationURL, entertainmentURL, streetInterviewURL;
+    var l1, l2, l3, l4, sequence;
+    var D1, D2, D3, D4, D5, D6, D7, D8, D9, D10, D11, D12, D13, D14, D15, D16, D17, D18, D19, D20, D21, D22, D23, D24;
+    for(var i in video){
+      
+    }
+  })*/
+  res.render('video/page', { title: '影音專區頁面' });
+})
+
 
 router.post('/add', isAdmin, function(req, res, next){
 
