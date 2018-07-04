691d935a79686ac035621698641ce4533adf8aaa
ingridpan123@gmail.com <ingridpan123@gmail.com>
2018-07-04 21:03:12 +0800

    做了一些後端

--- models/qna/qna.js
+++ models/qna/qna.js
@@ -1,10 +1,13 @@
 var mongoose = require('mongoose');
 
-var Question =new Schema({
+var Question = mongoose.Schema({
+    Username:String,//使用者名稱
     Title: String, //問題標題
     Type: String, //問題類型
     Content: String, //問題內容
-    CreateDate: Date
+    Answer:String,//問題的答案
+    CreateDate: Date,//創立日期
+    Click: Number//點擊次數
 });
 
-module.exports = mongoose.model( 'Question', question );
+module.exports = mongoose.model( 'Question', Question );
