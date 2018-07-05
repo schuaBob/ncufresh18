8300ce79bb5aa5faa055a1202d7cf3913187fb87
徐曼妮 <vanishment99@g.ncu.edu.tw>
2018-07-05 20:33:23 +0800

    網頁基本架構差不多 上傳功能

--- models/video/video.js
+++ models/video/video.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var video = mongoose.Schema({
     type: String,
     insert: String, //youtube嵌入碼
-    introduce: String //影片介紹圖片路徑
+    introduce: String //影片介紹圖片
 });
 
 module.exports = mongoose.model( 'video', video );
