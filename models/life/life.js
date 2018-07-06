6d6a68419ee127b17cf9e83abe79b05adae83b4b
eugenechou <eugene668822@yahoo.com.tw>
2018-07-06 16:39:56 +0800

    做了些css

--- models/life/life.js
+++ models/life/life.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 var life = mongoose.Schema({
     mainTitle   : String,
     subTitle    : [String],
-    type        : String
+    type        : String,
+    alt         : String
 });
 
 module.exports = mongoose.model('life', life);
