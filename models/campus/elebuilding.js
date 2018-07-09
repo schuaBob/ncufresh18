b816cdf5ed5e304271ef8a1f782abd39a57ae7f2
Bob Hua <schua1013@gmail.com>
2018-07-09 21:06:53 +0800

    you can now edit building elements

--- models/campus/elebuilding.js
+++ models/campus/elebuilding.js
@@ -4,7 +4,7 @@ var elebuilding = mongoose.Schema({
     Element_Name : String,
     Type : String,
     Element_Intro : String,
-    LastUpDate : Date
+    
 });
 
 module.exports = mongoose.model('elebuilding',elebuilding);
