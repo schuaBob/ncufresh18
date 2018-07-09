141a1e8f1c703084218e935f5122d111650278d1
徐曼妮 <vanishment99@g.ncu.edu.tw>
2018-07-09 21:17:31 +0800

    簡化程式碼 重設資料庫 加上一堆for

--- routes/video.js
+++ routes/video.js
@@ -4,282 +4,96 @@ var video = require('../models/video/video');
 
 /* 影音專區首頁 */
 router.get('/', function(req, res, next) {
-  /*video.find({type:"長劇"}).sort({id:-1}).exec(function(err, video) {
-    res.render('',{video:video});
+  /*video.find({type:"長劇"}).sort({id:1}).exec(function(err, video) {
+    res.render('',{longvideo:video});
   })
 
-  <% for(let i = 0;i<video;i++){ %>
-    <tr><%= video[i].fdsafkld %></tr>
+  <% for(let i = 0;i<longvideo;i++){ %>
+    <tr><%= longvideo[i].title %></tr>
   <% } %>
   let jjj = ["is","fdsaf","fdsaf"]
   let ppp = JSON.stringify(jjj)*/
 
 
+  // let job = [];
+  // job.push(video.find({type:"1"}).sort({id:-1}));
+  // job.push(video.find({type:"2"}).sort({id:-1}));
+  // job.push(video.find({type:"3"}).sort({id:-1}));
+  // Promise.all(job).then(function(video){
+  //   res.render('video/index', { 
+  //     title: '影音專區',
+  //     longvideo: video
+  //     video[0]
+  //     video[1]
+  //     video[3]
+  //   });
+  // }).catch(function(err){
+  //   if(err){
+  //     return next(err);
+  //   }
+  // })
 
 
-  video.find({}).exec(function(err, video) {
-    var l1_img, l2_img, l3_img, l4_img, l5_img;
-    var food_img, housing_img, transportation_img, education_img, entertainment_img, streetInterview_img;
-    var D1_img, D2_img, D3_img, D4_img, D5_img, D6_img, D7_img, D8_img, D9_img, D10_img, D11_img, D12_img, D13_img, D14_img, D15_img, D16_img, D17_img, D18_img, D19_img, D20_img, D21_img, D22_img, D23_img, D24_img, D25_img;
-    var food, housing, transportation, education, entertainment, streetInterview;
-    var l1, l2, l3, l4, l5;
-    var D1, D2, D3, D4, D5, D6, D7, D8, D9, D10, D11, D12, D13, D14, D15, D16, D17, D18, D19, D20, D21, D22, D23, D24, D25;
-    for(var i in video){
-      if(video[i].type == 'l1'){
-        l1 = video[i].insert;
-        l1_img = video[i].introduce;
-      }
-      if(video[i].type == 'l2'){
-        l2 = video[i].insert;
-        l2_img = video[i].introduce;
-      }
-      if(video[i].type == 'l3'){
-        l3 = video[i].insert;
-        l3_img = video[i].introduce;
-      }
-      if(video[i].type == 'l4'){
-        l4 = video[i].insert;
-        l4_img = video[i].introduce;
-      }
-      if(video[i].type == 'l5'){
-        l5 = video[i].insert;
-        l5_img = video[i].introduce;
-      }
-
-      else if(video[i].type == 'food'){
-        food = video[i].insert;
-        food_img = video[i].introduce;
-      }
-      else if(video[i].type == 'housing'){
-        housing = video[i].insert;
-        housing_img = video[i].introduce;
-      }
-      else if(video[i].type == 'transportation'){
-        transportation = video[i].insert;
-        transportation_img = video[i].introduce;
-      }
-      else if(video[i].type == 'education'){
-        education = video[i].insert;
-        education_img = video[i].introduce;
-      }
-      else if(video[i].type == 'entertainment'){
-        entertainment = video[i].insert;
-        entertainment_img = video[i].introduce;
-      }
-      else if(video[i].type == 'streetInterview'){
-        streetInterview = video[i].insert;
-        streetInterview_img = video[i].introduce;
-      }
-
-      else if(video[i].type == 'D1'){
-        D1 = video[i].insert;
-        D1_img = video[i].introduce;
-      }
-      else if(video[i].type == 'D2'){
-        D2 = video[i].insert;
-        D2_img = video[i].introduce;
-      }
-      else if(video[i].type == 'D3'){
-        D3 = video[i].insert;
-        D3_img = video[i].introduce;
-      }
-      else if(video[i].type == 'D4'){
-        D4 = video[i].insert;
-        D4_img = video[i].introduce;
-      }
-      else if(video[i].type == 'D5'){
-        D5 = video[i].insert;
-        D5_img = video[i].introduce;
-      }
-      else if(video[i].type == 'D6'){
-        D6 = video[i].insert;
-        D6_img = video[i].introduce;
-      }
-      else if(video[i].type == 'D7'){
-        D7 = video[i].insert;
-        D7_img = video[i].introduce;
-      }
-      else if(video[i].type == 'D8'){
-        D8 = video[i].insert;
-        D8_img = video[i].introduce;
-      }
-      else if(video[i].type == 'D9'){
-        D9 = video[i].insert;
-        D9_img = video[i].introduce;
-      }
-      else if(video[i].type == 'D10'){
-        D10 = video[i].insert;
-        D10_img = video[i].introduce;
-      }
-      else if(video[i].type == 'D11'){
-        D11 = video[i].insert;
-        D11_img = video[i].introduce;
-      }
-      else if(video[i].type == 'D12'){
-        D12 = video[i].insert;
-        D12_img = video[i].introduce;
-      }
-      else if(video[i].type == 'D13'){
-        D13 = video[i].insert;
-        D13_img = video[i].introduce;
-      }
-      else if(video[i].type == 'D14'){
-        D14 = video[i].insert;
-        D14_img = video[i].introduce;
-      }
-      else if(video[i].type == 'D15'){
-        D15 = video[i].insert;
-        D15_img = video[i].introduce;
-      }
-      else if(video[i].type == 'D16'){
-        D16 = video[i].insert;
-        D16_img = video[i].introduce;
-      }
-      else if(video[i].type == 'D17'){
-        D17 = video[i].insert;
-        D17_img = video[i].introduce;
-      }
-      else if(video[i].type == 'D18'){
-        D18 = video[i].insert;
-        D18_img = video[i].introduce;
-      }
-      else if(video[i].type == 'D19'){
-        D19 = video[i].insert;
-        D19_img = video[i].introduce;
-      }
-      else if(video[i].type == 'D20'){
-        D20 = video[i].insert;
-        D20_img = video[i].introduce;
-      }
-      else if(video[i].type == 'D21'){
-        D21 = video[i].insert;
-        D21_img = video[i].introduce;
-      }
-      else if(video[i].type == 'D22'){
-        D22 = video[i].insert;
-        D22_img = video[i].introduce;
-      }
-      else if(video[i].type == 'D23'){
-        D23 = video[i].insert;
-        D23_img = video[i].introduce;
-      }
-      else if(video[i].type == 'D24'){
-        D24 = video[i].insert;
-        D24_img = video[i].introduce;
-      }
-      else if(video[i].type == 'D25'){
-        D25 = video[i].insert;
-        D25_img = video[i].introduce;
-      }
-    }
-    res.render('video/index', { 
-      title: '影音專區',
-      l1: l1,
-      l1_img: l1_img,
-      l2: l2,
-      l2_img: l2_img,
-      l3: l3,
-      l3_img: l3_img,
-      l4: l4,
-      l4_img: l4_img,
-      l5: l5,
-      l5_img: l5_img,
-
-      food: food,
-      food_img: food_img,
-      housing: housing,
-      housing_img: housing_img,
-      transportation: transportation,
-      transportation_img: transportation_img,
-      education: education,
-      education_img: education_img,
-      entertainment: entertainment,
-      entertainment_img: entertainment_img,
-      streetInterview: streetInterview,
-      streetInterview_img: streetInterview_img,
-
-      D1: D1, D1_img: D1_img,
-      D2: D2, D2_img: D2_img,
-      D3: D3, D3_img: D3_img,
-      D4: D4, D4_img: D4_img,
-      D5: D5, D5_img: D5_img,
-      D6: D6, D6_img: D6_img,
-      D7: D7, D7_img: D7_img,
-      D8: D8, D8_img: D8_img,
-      D9: D9, D9_img: D9_img,
-      D10: D10, D10_img: D10_img,
-      D11: D11, D11_img: D11_img,
-      D12: D12, D12_img: D12_img,
-      D13: D13, D13_img: D13_img,
-      D14: D14, D14_img: D14_img,
-      D15: D15, D15_img: D15_img,
-      D16: D16, D16_img: D16_img,
-      D17: D17, D17_img: D17_img,
-      D18: D18, D18_img: D18_img,
-      D19: D19, D19_img: D19_img,
-      D20: D20, D20_img: D20_img,
-      D21: D21, D21_img: D21_img,
-      D22: D22, D22_img: D22_img,
-      D23: D23, D23_img: D23_img,
-      D24: D24, D24_img: D24_img,
-      D25: D25, D25_img: D25_img
-
+  video.find({type:"1"}).sort({id:-1}).exec(function(err, video1) {
+    video.find({type:"2"}).sort({id:-1}).exec(function(err, video2) {
+      video.find({type:"3"}).sort({id:-1}).exec(function(err, video3) { 
+        let video = video1.concat(video2).concat(video3);
+        res.render('video/index', { 
+          title: '影音專區',
+          longvideo: video1,
+          QAvideo: video2,
+          lifevideo: video3,
+          video: video
+        });
+      });
     });
   });
 });
 
 
 
-
 router.post('/add', function(req, res, next){
-  video.find({ type: req.body.type }, function(e, data) {
-    var insert, introduce;
+  let college, type, title;
+  type = req.body.title[0];
+  if(type === '1' || type === '3'){//中大長劇or悠遊中大
+      title = req.body.title.slice(1);
+  }
+  else if(type === '2'){//快問快答
+    college = 'c' + req.body.title[1];
+    title = req.body.title.slice(2);
+  }
+  
+  video.find({ title: title }, function(e, data) {
     if (data.length != 0) {
-      /*video.update({ type: req.body.type }, {
-        type: req.body.type,
-        insert: req.body.insert,
-        introduce: req.body.introduce
-      }, function(e) {
-        if (e) console.log(e);
-      });*/
-      insert = req.body.insert;
-      introduce = req.body.introduce;
-      if(insert != null && introduce == null){
-        video.update({ type: req.body.type }, {
-          type: req.body.type,
-          insert: req.body.insert,
-        }, function(e) {
-          if (e) console.log(e);
-        });
+      if(req.body.insert != ""){
+        video.update({title: title},{
+          title: title,
+          type: type,//中大長劇or悠遊中大or快問快答
+          college: college,//學院
+          insert: req.body.insert, //youtubeID
+        },function(e){
+          if(e) console.log(e);
+        })
       }
-      else if(insert == null && introduce != null){
-        video.update({ type: req.body.type }, {
-          type: req.body.type,
+      if(req.body.introduce != ""){
+        video.update({title: title},{
+          title: title,//中大長劇or悠遊中大or快問快答
           introduce: req.body.introduce
-        }, function(e) {
-          if (e) console.log(e);
-        });
-      }
-      else{
-        video.update({ type: req.body.type }, {
-          type: req.body.type,
-          insert: req.body.insert,
-          introduce: req.body.introduce
-        }, function(e) {
-          if (e) console.log(e);
-        });
+        },function(err){
+          if(e) console.log(e);
+        })
       }
       
     }else {
       new video({
-        type: req.body.type,
-        insert: req.body.insert,
-        introduce: req.body.introduce
+        title: title,
+        type: type,//中大長劇or悠遊中大 or快問快答
+        college: college,//學院
+        insert: req.body.insert, //youtubeID
+        introduce: req.body.introduce //影片介紹圖片
       }).save();
     }
   });
-  /*var form =  new formidable.IncomingForm();
-  var file_path;*/
 
   res.redirect('/video');
 })
