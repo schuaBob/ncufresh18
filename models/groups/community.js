da8cf55ccfce334d7a8ffe358561070f7ef7cd83
Hsuan0313 <kk40627@gmail.com>
2018-07-12 20:43:29 +0800

    可以跳出相對modal

--- models/groups/community.js
+++ models/groups/community.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 
 var community = mongoose.Schema({
-    type : String, // 性質類別
     name : String,  // 社群名稱
     introduction : String, // 社群介紹
     participation : String, // 加入社群
--- routes/groups.js
+++ routes/groups.js
@@ -8,7 +8,7 @@ var student = require('../models/groups/student');
 
 /* 系所社團首頁 */
 router.get('/', function(req, res, next) {
-  res.render('groups/index', { title: '系所社團' });
+  res.render('groups/index', { title: '系所社團', user: req.user });
 });
 
 router.get('/department', function(req, res, next) {
@@ -21,20 +21,10 @@ router.get('/department', function(req, res, next) {
   });
 });
 
-router.get('/club', function(req, res, next) { 
-  club.find({}).exec(function(err, club) {
-    res.render('groups/club', {
-      title: '社團 ',
-      user: req.user,
-      club: club,    
-    });
-  });
-});
-
 router.get('/community', function(req, res, next) { 
   community.find({}).exec(function(err, community) {
     res.render('groups/community', {
-      title: '社群 ',
+      title: '其他學生組織 ',
       community: community,    
     });
   });
@@ -50,23 +40,80 @@ router.get('/student', function(req, res, next) {
 });
 
 
-  router.post('/edit_club', function(req, res, next) {
-    club.update({  
-      typenum: req.body.typenum,
-      name: req.body.name,
-      introduction: req.body.introduction,
-      participation: req.body.participation,
-      FB: req.body.FB,
-      picture: req.body.picture,
-    }, function(err, doc) {
-      if (err) {
-      return next(err)
-      }
-      else 
-      res.redirect('/groups/club');
+
+/* 原本想用typelearn.play.date.service分類，後來不用那麼麻煩 */
+router.get('/club', function(req, res, next) {
+  club.find({type:"1"}).exec(function(err, club1) {
+    club.find({type:"2"}).exec(function(err, club2) {
+      club.find({type:"3"}).exec(function(err, club3) { 
+        club.find({type:"4"}).exec(function(err, club4) {
+          let club = club1.concat(club2).concat(club3).concat(club4);
+          res.render('groups/club', {
+            title: '社團',
+            typelearn: club1,
+            typeplay: club2,
+            typedate: club3,
+            typeservice: club4,
+            club: club,
+          });
+        });
+      });
     });
   });
+});
 
+
+/*新增編輯社團 */
+router.post('/add_club', function(req, res, next) {
+  let typenum = req.body.type[0];
+  name = req.body.name;
+  club.find({name: name}, function(err, data) {
+    if (data.length == 0) {
+      new club ({
+        type: typenum,
+        name: req.body.name,
+        introduction: req.body.introduction,
+        participation: req.body.participation,
+        FB: req.body.FB,
+        picture: req.body.picture,
+      }).save();
+    }
+    else{
+      data[0].introduction = req.body.introduction;
+      data[0].participation = req.body.participation;
+      data[0].FB = req.body.FB;
+      data[0].picture = req.body.picture;
+      data[0].save() ; 
+    }
+  });
+  res.redirect('/groups/club');
+});
+
+router.post('/add_community', function(req, res, next) {
+  name = req.body.name;
+  community.find({name: name}, function(err, data) {
+    if (data.length == 0) {
+      new community ({
+        name: req.body.name,
+        introduction: req.body.introduction,
+        participation: req.body.participation,
+        FB: req.body.FB,
+        picture: req.body.picture,
+      }).save();
+    }
+    else{
+      data[0].introduction = req.body.introduction;
+      data[0].participation = req.body.participation;
+      data[0].FB = req.body.FB;
+      data[0].picture = req.body.picture;
+      data[0].save() ; 
+    }
+  });
+  res.redirect('/groups/community');
+});
+  
+
+/* 學生會只有一項需要編輯所以直接update就可 */
   router.post('/edit_student', function(req, res, next) {
     student.update({
       introduction: req.body.introduction,
@@ -82,21 +129,6 @@ router.get('/student', function(req, res, next) {
   });
   
 
-  router.post('/add_club', function(req, res, next) {
-    var cb = new club ({
-      typenum: req.body.typenum,
-      name: req.body.name,
-      introduction: req.body.introduction,
-      participation: req.body.participation,
-      FB: req.body.FB,
-      picture: req.body.picture,
-    }).save(function(err, doc) {
-      if (err) {
-        return next(err)
-      }
-      else 
-      res.redirect('/groups/club');
-    });
-  });
+  
 
 module.exports = router;
