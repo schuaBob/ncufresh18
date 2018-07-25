244f070ea288619b8446c88672321a41fdea9d03
agenga <agenga.chaos@gmail.com>
2018-07-25 19:54:39 +0800

    pers

--- routes/personal.js
+++ routes/personal.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer') ;
+
 
 /* 個人專區首頁 */
 router.get('/', function(req, res, next) {
   res.render('personal/index', { title: '個人專區' ,user : req.user});
 });
+
+router.post('/editPicture' ,upload.single('picture') , function(req,res ,next){
+  var cuted = req.file.path.split("/"),
+      pathed = cuted[2] + "/" + cuted[3];
+  var newPicture = new picture({
+      number    : req.body.mainTitle,
+      path      : pathed
+  }).save(function(err,doc){
+      if(err){ return next(err);}
+      console.log(doc);
+      res.redirect('back');
+  });
+})
 
 module.exports = router;
