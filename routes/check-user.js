0e9eccf3fc6520eb7a0ef5e7882126ac35b62b41
Ming-Han Yang <soto2080@gmail.com>
2018-07-27 14:13:04 +0800

    Login Detect

--- routes/check-user.js
+++ routes/check-user.js
@@ -1,17 +1,10 @@
 module.exports = {
     isLoggedIn :
         function(req, res, next) {
-<<<<<<< HEAD
-            if (req.isAuthenticated()) {
-	   	return	 next();
-	}
-        return res.redirect('/');
-=======
             if (!req.isAuthenticated()) {
                 return res.redirect('/');
             }
             next();
->>>>>>> origin/smallgame
         },
     isAdmin :
         function(req, res, next) {
