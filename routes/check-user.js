ff10aa0a12e49c7f7d2bab9fde95885646d9deeb
dd233 <80776877@qq.com>
2018-08-07 07:18:31 +0800

    修改沒登入跳轉

--- routes/check-user.js
+++ routes/check-user.js
@@ -2,7 +2,7 @@ module.exports = {
     isLoggedIn :
         function(req, res, next) {
             if (!req.isAuthenticated()) {
-                return res.redirect('/login/');
+                return res.redirect('/auth/provider');
             }
             next();
         },
